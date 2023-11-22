867b8505b98efe762615212d74877c5da3fafdfd
Mipo <124940663+miaspe@users.noreply.github.com>
2023-11-22 13:05:02 -0300

    Update Produto.java

--- CadastroEE/CadastroEE-ejb/src/java/cadastroee/model/Produto.java
+++ CadastroEE/CadastroEE-ejb/src/java/cadastroee/model/Produto.java
@@ -11,10 +11,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-/**
- *
- * @author AlarconAbap
- */
+
 @Entity
 @Table(name = "PRODUTO")
 @NamedQueries({
